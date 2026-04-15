e37d21af3ecb0cd0caa78d3621ba0a7aff9a215e
Visvajiit <vchezhian@wpi.edu>
2026-04-15 12:50:12 -0400

    assignment 2

--- launch/scara_gazebo.launch.py
+++ launch/scara_gazebo.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess, TimerAction, IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from ament_index_python.packages import get_package_share_directory
@@ -11,59 +11,24 @@ def generate_launch_description():
         get_package_share_directory('scara_robot'),
         'urdf', 'scara_modified.urdf.xacro'
     )
-
     robot_description = Command(['xacro ', urdf_file])
-    
-    # Get gazebo_ros launch directory
     gazebo_ros_dir = get_package_share_directory('gazebo_ros')
 
     return LaunchDescription([
-    
-        # Launch Gazebo using gazebo_ros built-in launch file
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(gazebo_ros_dir, 'launch', 'gazebo.launch.py')
             )
         ),
-
-        # Publish robot description
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             parameters=[{'robot_description': robot_description}]
         ),
-
-        # Spawn robot in Gazebo
         Node(
             package='gazebo_ros',
             executable='spawn_entity.py',
             arguments=['-topic', 'robot_description', '-entity', 'scara_robot'],
             output='screen'
-        ),
-
-        # Load and start joint state broadcaster
-        TimerAction(
-            period=3.0,
-            actions=[
-                Node(
-                    package='controller_manager',
-                    executable='spawner',
-                    arguments=['joint_state_broadcaster'],
-                    output='screen'
-                )
-            ]
-        ),
-
-        # Load and start scara controller
-        TimerAction(
-            period=5.0,
-            actions=[
-                Node(
-                    package='controller_manager',
-                    executable='spawner',
-                    arguments=['joint_trajectory_controller'],
-                    output='screen'
-                )
-            ]
         ),
     ])
--- scara_robot/joint_effort_controller.py
+++ scara_robot/joint_effort_controller.py
@@ -4,7 +4,9 @@ from sensor_msgs.msg import JointState
 from gazebo_msgs.srv import ApplyJointEffort
 from builtin_interfaces.msg import Duration, Time
 from controller_manager_msgs.srv import SwitchController
-from std_srvs.srv import Empty
+import subprocess
+import os
+from scara_interfaces.srv import SetRef
 
 class JointEffortController(Node):
     def __init__(self):
@@ -13,34 +15,27 @@ class JointEffortController(Node):
         self.joint_sub = self.create_subscription(JointState, '/joint_states', self.joint_state_callback, 10)
 
         self.effort_client = self.create_client(ApplyJointEffort, '/apply_joint_effort')
-        self.switch_client = self.create_client(SwitchController, '/controller_manager/switch_controller')
-        self.unpause_client = self.create_client(Empty, '/unpause_physics')
-
-        # Service client to apply effort in Gazebo
-        self.effort_client = self.create_client(ApplyJointEffort, '/apply_joint_effort')
+        self.srv = self.create_service(SetRef,'set_reference',self.set_ref_cb) # custom service- inside scara_interfaces.srv
         self.joint_positions = {}
-        # Run startup sequence
-        self.startup_sequence()
+        
 
+        self.prev_error=0.0
+        self.prev_pos = -2.5
 
-    def startup_sequence(self):
-        # 1. Unpause physics
-        self.get_logger().info('Unpausing physics...')
-        self.unpause_client.wait_for_service(timeout_sec=5.0)
-        future = self.unpause_client.call_async(Empty.Request())
-        rclpy.spin_until_future_complete(self, future)
-        self.get_logger().info('Physics unpaused')
+        self.prev_time=self.get_clock().now().nanoseconds * 1e-9
+        self.log_file = open('joint_log.txt','w')
+        self.kp=500 # porportional gain
+        self.kd=45 # derivative gain
+        self.ref=-1 # setting reference value to match 
 
-        # 2. Deactivate trajectory controller
-        self.get_logger().info('Deactivating trajectory controller...')
-        self.switch_client.wait_for_service(timeout_sec=5.0)
-        req = SwitchController.Request()
-        req.activate_controllers = []
-        req.deactivate_controllers = ['joint_trajectory_controller']
-        req.strictness = 1
-        future = self.switch_client.call_async(req)
-        rclpy.spin_until_future_complete(self, future)
-        self.get_logger().info('Trajectory controller deactivated — ready to send efforts')
+    
+    
+    def set_ref_cb(self, request, response):
+        self.ref=request.position # takes position and stores it for the node to process
+        response.success = True #
+        self.get_logger().info(f'New reference: {request.position}')
+        return response
+
 
     def joint_state_callback(self, msg):
         # Store latest joint positions by name
@@ -49,9 +44,35 @@ class JointEffortController(Node):
 
         pos = self.joint_positions.get('joint3', None)
         if pos is not None:
-            self.get_logger().info(f'joint3 position: {pos:.4f}')
-            self.apply_effort('joint3', effort= 5000.0)
+            
+            now=self.get_clock().now().nanoseconds * 1e-9 # take current time 
+            dt=now- self.prev_time 
 
+            error = self.ref-pos #finding error derivative
+            if dt>0:
+                error_dot=(error-self.prev_error) /dt
+            else:
+                error_dot = 0.0
+            effort=self.kp*error + self.kd*error_dot
+            
+            self.prev_error = error
+            self.prev_time = now
+            t = self.get_clock().now().nanoseconds*1e-9 # getting time to visualize ing matlab
+            self.log_file.write(f'{t},{self.ref},{pos}\n') # log the reference and current position at that instance
+            
+            
+            self.apply_effort('joint3', effort)
+            self.get_logger().info(f'joint3 position: {pos:.4f}')    
+    def apply_effort(self, joint_name, effort):
+        env = os.environ.copy()
+        env['GAZEBO_MASTER_URI'] = 'http://localhost:11345'
+        subprocess.Popen(
+            ['/usr/bin/gz', 'joint', '-m', 'scara_robot', '-j', joint_name, '-f', str(effort)],
+            env=env,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )       
+"""
     def apply_effort(self, joint_name, effort):
         if not self.effort_client.service_is_ready():
             self.get_logger().warn('Service not ready')
@@ -61,11 +82,13 @@ class JointEffortController(Node):
         req.joint_name = joint_name
         req.effort = effort
         req.start_time = Time(sec=0, nanosec=0)
-        req.duration = Duration(sec=50, nanosec=100_000_000)
+        req.duration = Duration(sec=0, nanosec=10_000_000)
+
 
         # Async call — doesn't block the subscriber
         self.effort_client.call_async(req)
-
+        """
+    
 
 def main():
     rclpy.init()
